5f76787e173afcd4225e7e97068ce405d4e36e30
groofnish <xcwulei@163.com>
2024-06-27 23:27:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,8 +9,8 @@ var config = {
         "我想你会一直在我心里吧",
     ],
     imgs: {
-        "生日快乐": "./imgs/love.jpg",
-        "今天是你的生日": "./imgs/birthday.jpg",
+        "很幸运能在我生命中遇见你": "./imgs/love.jpg",
+        //"今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
